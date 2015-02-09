7abd5b638c7ee3d5674edd7b41a8589a1dbbf7ae
E.G. ismaail <contact@ismaail.com>
2015-02-09 20:51:02 +0000

    Add grunt tasks to compile and watch "scss/" folder

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/*jshint strict:true, browser:true, jquery:true, devel:true, curly:true, eqeqeq:true, immed:true, latedef:true, plusplus:true, undef:true, unused:true, laxbreak:true, nonew:false */
+/*global module */
+
+module.exports = function(grunt) {
+	"use strict";
+
+	grunt.initConfig({
+		compass: {
+			dist: {
+				options: {
+					httpPath: '/',
+					sassDir: 'scss',
+					cssDir: 'public/css',
+					sourcemap: true
+				}
+			}
+		},
+		watch: {
+			scss: {
+				files: ['scss/**'],
+				tasks: ['compass'],
+				options: {
+					interrupt: true
+				}
+			}
+		},
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+
+	grunt.registerTask("default", []);
+};
